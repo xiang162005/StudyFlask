b1eb326ddacd569a4b22e060e5fedf514da90157
xiang162005 <xiang162005@126.com>
2019-07-13 22:46:21 +0800

    第二版的示例2-2

--- hello.py
+++ hello.py
@@ -7,7 +7,4 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!<h1>' % name
-
-if __name__ == '__main__':
-     app.run(debug=True)
+    return '<h1>Hello, {}!<h1>'.format(name)
