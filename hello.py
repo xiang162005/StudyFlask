8b768ffe50702cd9f8db6f07210a3dc658b21756
xiang162005 <xiang162005@126.com>
2019-07-14 15:24:42 +0800

    第二版的示例4-5

--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -10,7 +8,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
-moment = Moment(app)
 
 
 class NameForm(FlaskForm):
@@ -30,9 +27,8 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
