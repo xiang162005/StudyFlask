c6f47fb81f1d3f5db0162c438eb31ddac8cd5d4b
xiang162005 <xiang162005@126.com>
2019-07-14 11:04:23 +0800

    第二版的示例3-5

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template
-from flask_script import Manager
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-manager = Manager(app)
+
 bootstrap = Bootstrap(app)
 
 @app.route('/')
@@ -13,6 +12,3 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
-if __name__ == '__main__':
-     manager.run()
