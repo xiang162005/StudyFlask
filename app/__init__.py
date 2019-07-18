4fc8436bd10b51696ef27343908761f448cc5978
xiang162005 <xiang162005@126.com>
2019-07-18 14:25:27 +0800

    第二版的示例8-5

--- app/__init__.py
+++ app/__init__.py
@@ -27,6 +27,8 @@ def create_app(config_name):
 
     # 添加路由和自定义的错误界面
     from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
