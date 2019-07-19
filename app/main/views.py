b996f2ffddcd08d578c46890ad37cb3de4a9e405
xiang162005 <xiang162005@126.com>
2019-07-19 18:39:31 +0800

    第二版的示例11-4

--- app/main/views.py
+++ app/main/views.py
@@ -2,14 +2,22 @@ from flask import render_template, redirect, url_for, abort, flash
 from flask_login import login_required, current_user
 from . import main
 from .. import db
-from .forms import EditProfileForm, EditProfileAdminForm
-from ..models import User, Role
+from .forms import EditProfileForm, EditProfileAdminForm, PostForm
+from ..models import User, Role, Permission, Post
 from ..decorators import admin_required
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = PostForm()
+    if current_user.can(Permission.WRITE) and form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', form=form, posts=posts)
 
 
 @main.route('/user/<username>')
