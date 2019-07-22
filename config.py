d67b0c6566cf9473c2c45bf310eaeaf150c72bf7
xiang162005 <xiang162005@126.com>
2019-07-22 20:15:16 +0800

    第二版的示例15-4

--- config.py
+++ config.py
@@ -30,6 +30,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
+    WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
